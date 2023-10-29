020584fd8a8e6679c8033728a02236968f9b9120
Yash-Ambekar <yashambekar03@gmail.com>
2023-10-30 02:20:56 +0530

    Added OAuth using Supabase

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,14 +1,25 @@
 import Categories from "@/components/Categories";
 import TopProducts from "@/components/TopProducts";
 import Contact from "@/components/Contact";
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Session, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { Database } from "../@types/supabase";
+import { useEffect } from "react";
 
-export default function Home() {
+
+export default async function Home() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const {data:{session}}= await supabase.auth.getSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-y-20">
+      <Navbar session={session as Session}/>
       <Categories />
       <TopProducts />
       <Contact />
+      <Footer />
     </main>
   );
 }
