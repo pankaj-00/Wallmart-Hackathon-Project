ecbd29a90ec26a7e02a379ee36b94c4dfe20f9b7
ParthMakode <makodeparth15@gmail.com>
2023-11-06 01:22:53 +0530

    integrated lovo ai . logo remaining

--- flutter-app/lib/home_page.dart
+++ flutter-app/lib/home_page.dart
@@ -1,7 +1,12 @@
+import 'package:appapap/api.dart';
+import 'package:appapap/speech-api.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:appapap/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'chatbubble.dart';
+// ...
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +15,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final speechToText = SpeechToText();
+  SpeechToText speechToText = SpeechToText();
   String lastWords = '';
-
+  String reply = '';
+  String audioUrl='';
+  bool isListening = false;
   @override
   void initState() {
     super.initState();
@@ -34,12 +41,22 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> startListening() async {
     await speechToText.listen(onResult: onSpeechResult);
-    setState(() {});
+    setState(() {
+      print("2");
+    });
   }
 
   Future<void> stopListening() async {
     await speechToText.stop();
-    setState(() {});
+
+    reply=await fetchResponse(lastWords);
+    print("sending to lovo");
+    audioUrl =await returnAudio(reply);
+    var player=AudioPlayer();
+    await player.play(UrlSource(audioUrl));
+    setState(() {
+
+    });
   }
 
   @override
@@ -111,43 +128,92 @@ class _HomePageState extends State<HomePage> {
         ),
         Column(
           children: [
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-              decoration: const BoxDecoration(
-                  color: Colors.amberAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              height: 80,
-              child: const Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  "convAI agent",
-                  style: TextStyle(fontSize: 25, fontFamily: "Cera Pro"),
-                ),
-              ),
-            )
-          ],
-        ),
-        Text(lastWords)
-      ]),
-      floatingActionButton: FloatingActionButton(
+        //     Container(
+        //       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+        //       decoration: const BoxDecoration(
+        //           color: Colors.amberAccent,
+        //           borderRadius: BorderRadius.all(Radius.circular(15))),
+        //       height: 80,
+        //       child: const Padding(
+        //         padding: EdgeInsets.all(20),
+        //         child: Text(
+        //           "convAI agent",
+        //           style: TextStyle(fontSize: 25, fontFamily: "Cera Pro"),
+        //         ),
+        //       ),
+        //     )
+        //   ],
+        // ),
+
+            ChatBubble(
+              isUser: true,
+              message: lastWords,
+              // height: 60,
+
+              color: Theme.of(context).scaffoldBackgroundColor,
+              padding: const EdgeInsets.all(10),
+              borderRadius: BorderRadius.circular(20),
+              showShadow: false,
+            ),
+            ChatBubble(
+              isUser: false,
+              message: reply,
+              // height: 60,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              padding: const EdgeInsets.all(10),
+              borderRadius: BorderRadius.circular(20),
+              showShadow: false,
+            ),
+
+
+          ]),]),
+        floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () async {
-          if (await speechToText.hasPermission && speechToText.isNotListening) {
-            // print('1');
-            startListening();
-            print('1');
-          } else if (speechToText.isListening) {
-            stopListening();
-            print('2');
-            setState(() {});
-          } else {
-            initSpeechToText();
-            print('3');
-          }
+        onPressed: () {
+          print("onpressed");
         },
-        child: const Icon(Icons.mic),
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  startListening();
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            stopListening();
+          },
+          child: Icon(Icons.mic),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+
+//backup button logic
+// onPressed: () async {
+//   if (await speechToText.hasPermission && speechToText.isNotListening) {
+//     // print('1');
+//     startListening();
+//     print('1');
+//   } else if (speechToText.isListening) {
+//     stopListening();
+//
+//     print('2');
+//     // setState(()async {
+//     //   final player = AudioPlayer();
+//     //   await player.play(UrlSource(reply));
+//     // });
+//   } else {
+//     initSpeechToText();
+//     print('3');
+//   }
+// },
